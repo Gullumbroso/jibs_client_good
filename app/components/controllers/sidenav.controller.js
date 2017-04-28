9483ce4232ee438efcabf201eb7f4fbbecb98c0c
laure-s <laurescemama@gmail.com>
2017-04-28 10:24:35 +0300

    sidebar with shadow

--- app/components/controllers/sidenav.controller.js
+++ app/components/controllers/sidenav.controller.js
@@ -12,10 +12,23 @@ angular
         TYPES = ['Action', '$rootScope', 'Triggers', 'People', 'Time', 'Venue'];
 
         $scope.statesList = ["State", "State", "State"];
+        $scope.actionIconDict = {"mail": "mail_outline", "uploud": "file_upload", "download": "file_download",
+        "alarm":"alarm", "event":"date_range"};
+        $scope.actionStrDict = {"mail": "Send a mail", "uploud": "Upload a file", "download": "Download a file",
+            "alarm":"Set an alarm", "event":"Create an event"};
+
 
         $scope.toggleStick = function () {
             $scope.stick = !$scope.stick;
             return $scope.stick;
+        };
+
+        $scope.statesColorList = ["#f6105e", "#ffb60b", "#5c30f9"];
+        $scope.colorIndex = 0;
+        $scope.generateState = function () {
+
+            // generate state
+            $scope.colorIndex = ($scope.colorIndex+1)%3;
         };
 
         function buildOpener(componentId) {
